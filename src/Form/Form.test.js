aa5ac7275925aaad8511c6a9c0d2a0e8f5697473
Kimala Cochran <kimalajoy@gmail.com>
2020-05-21 12:04:41 -0600

    started another test for form component

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import Form from './ReservationCards'
+import Form from './Form'
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Form', () => {
-  it('', () => {
+  it('should render correctly', () => {
     
+    const {getByPlaceholderText} = render(
+    <Form />)
+
+  expect(getByPlaceholderText('name')).toBeInTheDocument();
+  expect(getByPlaceholderText('date')).toBeInTheDocument();
+  expect(getByPlaceholderText('time')).toBeInTheDocument();
+  expect(getByPlaceholderText('number of guests')).toBeInTheDocument();
+  });
+  
+  it('should display what is entered in the box', () => {
+    const {ByDisplayValue} = render(
+    <Form 
+    />)
+
+
+    // expect(ByDisplayValue('beans')).toBeInTheDocument();
   });
   
 });
