3160be2bbafa09c06efc30e0603d2f6bc1e00e8b
Kimala Cochran <kimalajoy@gmail.com>
2020-05-21 09:12:04 -0600

    Initial commit

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
+        <h1 className='app-title'>Turing Cafe Reservations!</h1>
         <div className='resy-form'>
 
         </div>
